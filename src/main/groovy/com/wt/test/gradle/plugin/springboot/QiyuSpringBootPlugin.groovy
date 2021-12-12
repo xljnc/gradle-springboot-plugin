ff42e350a4edf3c8161e0f0ff9a36b77472f770d
qiyu <jsj0804wt@126.com>
2021-12-12 22:49:20 +0800

    springboot插件 by qiyu 2021-12-12

--- src/main/groovy/com/wt/test/gradle/plugin/springboot/QiyuSpringBootPlugin.groovy
+++ src/main/groovy/com/wt/test/gradle/plugin/springboot/QiyuSpringBootPlugin.groovy
@@ -2,6 +2,7 @@ package com.wt.test.gradle.plugin.springboot
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
 
 /**
  *
@@ -9,78 +10,63 @@ import org.gradle.api.Project
  * @author 一贫
  * @date 2021/12/10
  */
- class QiyuSpringBootPlugin implements Plugin<Project>{
+class QiyuSpringBootPlugin implements Plugin<Project> {
 
-     static final String EXTENSION_NAME = "qiyuSpringBoot";
+    static final String EXTENSION_NAME = "qiyuSpringBoot";
 
-     @Override
-     void apply(Project project) {
-         project.extensions.create(EXTENSION_NAME, SpringBootPluginExtension);
-         project.repositories{
-             mavenLocal()
-             maven { name "Alibaba"; url "https://maven.aliyun.com/repository/public" }
-             mavenCentral()
-         }
-         project.buildscript {
-             ext {
-                 sourceCompatibility = 11
-                 archiveClassifier = ''
-                 springbootVersion = '2.6.1'
-                 dependencyManagementPluginVersion = '1.0.11.RELEASE'
-             }
-             repositories {
-                 mavenLocal()
-                 maven { name "Alibaba"; url "https://maven.aliyun.com/repository/public" }
-                 mavenCentral()
-             }
-             dependencies {
-                 classpath "org.springframework.boot:spring-boot-gradle-plugin:${springbootVersion}"
-                 classpath 'io.spring.gradle:dependency-management-plugin:${dependencyManagementPluginVersion}'
-             }
-         }
-         project.apply{
-             it.plugin("java")
-             it.plugin("java-library")
-             it.plugin("io.spring.dependency-management")
-             it.plugin("maven-publish")
-         }
+    @Override
+    void apply(Project project) {
+        def qiyuSpringBoot = project.extensions.create(EXTENSION_NAME, SpringBootPluginExtension);
+        project.repositories {
+            mavenLocal()
+            maven { name "Alibaba"; url "https://maven.aliyun.com/repository/public" }
+            mavenCentral()
+        }
 
-         project.subprojects {
-             group group
-             version version
-             sourceCompatibility = ext.sourceCompatibility
+        project.allprojects.each {
+            it.group project.getGroup()
+            it.version project.getVersion()
 
-             apply plugin: 'java'
-             apply plugin: 'io.spring.dependency-management'
-             apply plugin: 'org.springframework.boot'
-             apply plugin: 'java-library'
-             apply plugin: 'maven-publish'
+            it.ext {
+                sourceCompatibility = qiyuSpringBoot.sourceCompatibility
+            }
 
-             bootJar {
-                 enabled = false
-             }
-         }
+            it.repositories {
+                mavenLocal()
+                maven { name "Alibaba"; url "https://maven.aliyun.com/repository/public" }
+                mavenCentral()
+            }
 
-         project.allprojects {
-             jar {
-                 archiveClassifier = ext.archiveClassifier
-             }
+            it.apply plugin: 'java'
+            it.apply plugin: 'java-library'
+            it.apply plugin: 'maven-publish'
+            it.apply plugin: 'io.spring.dependency-management'
 
-             publishing {
-                 publications {
-                     mavenLocal(MavenPublication) {
-                         from components.java
-                         versionMapping {
-                             usage('java-api') {
-                                 fromResolutionOf('runtimeClasspath')
-                             }
-                             usage('java-runtime') {
-                                 fromResolutionResult()
-                             }
-                         }
-                     }
-                 }
-             }
-         }
-     }
- }
+            it.getTasks().each { task ->
+                if (task.name == 'jar')
+                    task.setProperty("archiveClassifier", qiyuSpringBoot.archiveClassifier)
+            }
+
+            def publicationsClosure = {
+                maven(MavenPublication) {
+                    groupId project.group
+                    version project.version
+//                    from components.java
+                }
+            }
+            publicationsClosure.setDelegate(project)
+            it.publishing.publications(publicationsClosure)
+        }
+
+        project.subprojects.each {
+            it.group project.getGroup()
+            it.version project.getVersion()
+
+            it.apply plugin: 'org.springframework.boot'
+            it.getTasks().each {
+                if (it.name == 'bootJar')
+                    it.setProperty("enabled", false)
+            }
+        }
+    }
+}
