1d981fffff66474cea4dec7118352963208a47c1
yipin.wt <yipin.wt@cfuture.shop>
2021-12-24 10:21:37 +0800

    插件更新 by qiyu 2021-12-24

--- src/main/groovy/com/wt/test/gradle/plugin/springboot/QiyuSpringBootPlugin.groovy
+++ src/main/groovy/com/wt/test/gradle/plugin/springboot/QiyuSpringBootPlugin.groovy
@@ -1,19 +1,11 @@
 package com.wt.test.gradle.plugin.springboot
 
-import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
-import io.spring.gradle.dependencymanagement.internal.bridge.InternalComponents
-import io.spring.gradle.dependencymanagement.maven.PomDependencyManagementConfigurer
-import org.gradle.api.Action
+
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
-import org.gradle.api.internal.artifacts.repositories.resolver.MavenResolver
 import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.api.tasks.Upload
 import org.gradle.api.tasks.bundling.Jar
 
 /**
@@ -26,22 +18,10 @@ class QiyuSpringBootPlugin implements Plugin<Project> {
 
     static final String EXTENSION_NAME = "qiyuSpringBoot";
 
-    static final String DEPENDENCY_MANAGEMENT_EXTENSION_NAME = "dependencyManagement";
-
     @Override
     void apply(Project project) {
-        project.apply plugin: 'java'
-        project.apply plugin: 'io.spring.dependency-management'
 
         def qiyuSpringBoot = project.extensions.create(EXTENSION_NAME, SpringBootPluginExtension);
-
-//        InternalComponents internalComponents = new InternalComponents(project);
-//
-//        final DependencyManagementExtension dependencyManagementExtension =
-//                internalComponents.getDependencyManagementExtension();
-//
-//        project.extensions.add(DEPENDENCY_MANAGEMENT_EXTENSION_NAME, dependencyManagementExtension);
-
 
         project.repositories {
             mavenLocal()
@@ -54,8 +34,8 @@ class QiyuSpringBootPlugin implements Plugin<Project> {
         }
 
         project.subprojects.each {
-            it.group project.getGroup()
-            it.version project.getVersion()
+            it.group project.group
+            it.version project.version
 
             it.extensions.extraProperties.set("sourceCompatibility", qiyuSpringBoot.sourceCompatibility == null ? 11 : qiyuSpringBoot.sourceCompatibility)
 
@@ -65,14 +45,18 @@ class QiyuSpringBootPlugin implements Plugin<Project> {
                 mavenCentral()
             }
 
+            it.buildscript {
+                dependencies {
+                    classpath "org.springframework.boot:spring-boot-gradle-plugin:2.6.1"
+                    classpath "io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE"
+                }
+            }
+
             it.apply plugin: 'java'
             it.apply plugin: 'java-library'
             it.apply plugin: 'org.springframework.boot'
             it.apply plugin: 'maven-publish'
             it.apply plugin: 'io.spring.dependency-management'
-
-            it.dependencyManagement = it.rootProject.dependencyManagement
-//            configDependencyManagement(it, dependencyManagementExtension);
 
             it.getTasks().each { task ->
                 if (task.name == 'jar') {
@@ -114,76 +98,5 @@ class QiyuSpringBootPlugin implements Plugin<Project> {
         }
         publicationsClosure.setDelegate(project)
         project.publishing.publications(publicationsClosure)
-    }
-
-    private void configDependencyManagement(Project subProject, DependencyManagementExtension dependencyManagementExtension) {
-        InternalComponents internalComponents = new InternalComponents(subProject);
-
-        subProject.extensions.add(DEPENDENCY_MANAGEMENT_EXTENSION_NAME, dependencyManagementExtension);
-
-        internalComponents.createDependencyManagementReportTask("dependencyManagement");
-
-        subProject.getConfigurations().all(internalComponents.getImplicitDependencyManagementCollector());
-        subProject.getConfigurations().all(internalComponents.getDependencyManagementApplier());
-
-        configurePomCustomization(subProject, dependencyManagementExtension);
-    }
-
-    private void configurePomCustomization(final Project project, DependencyManagementExtension dependencyManagementExtension) {
-        final PomDependencyManagementConfigurer pomConfigurer = dependencyManagementExtension.getPomConfigurer();
-        project.getTasks().withType(Upload.class, new Action<Upload>() {
-
-            @Override
-            public void execute(final Upload upload) {
-                upload.doFirst(new Action<Task>() {
-
-                    @Override
-                    public void execute(Task task) {
-                        upload.getRepositories().withType(MavenResolver.class, new Action<MavenResolver>() {
-
-                            @Override
-                            public void execute(MavenResolver mavenResolver) {
-                                mavenResolver.getPom().withXml(pomConfigurer);
-                            }
-
-                        });
-                    }
-
-                });
-
-            }
-
-        });
-        project.getPlugins().withType(MavenPublishPlugin.class, new Action<MavenPublishPlugin>() {
-
-            @Override
-            public void execute(MavenPublishPlugin mavenPublishPlugin) {
-                configurePublishingExtension(project, pomConfigurer);
-            }
-
-        });
-    }
-
-    private void configurePublishingExtension(Project project, final PomDependencyManagementConfigurer extension) {
-        project.getExtensions().configure(PublishingExtension.class, new Action<PublishingExtension>() {
-
-            @Override
-            public void execute(PublishingExtension publishingExtension) {
-                configurePublications(publishingExtension, extension);
-            }
-
-        });
-    }
-
-    private void configurePublications(PublishingExtension publishingExtension,
-                                       final PomDependencyManagementConfigurer extension) {
-        publishingExtension.getPublications().withType(MavenPublication.class, new Action<MavenPublication>() {
-
-            @Override
-            public void execute(MavenPublication mavenPublication) {
-                mavenPublication.getPom().withXml(extension);
-            }
-
-        });
     }
 }
